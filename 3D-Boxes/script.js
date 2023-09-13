d88da607210c877e5ee4d5f6b93d2906112b969c
ShankarAdd <shankarraibagi777>
2023-09-13 14:20:36 +0530

    comments

--- 3D-Boxes/script.js
+++ 3D-Boxes/script.js
@@ -1,8 +1,11 @@
+//Creating element to store the boxes
 const boxContainer=document.getElementById('boxes');
 const btn=document.getElementById('btn');
 
+//when the magic button clicked we create the small class for transition
 btn.addEventListener('click', () => boxContainer.classList.toggle('small'))
 
+//creating the boxes and adding it one by one 
 function createBoxes(){
     for(let i=0; i<4 ;i++){
         for(let j=0; j<4 ;j++){
